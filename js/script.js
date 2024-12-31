b686a3249561c77ddc30163d577be497c8259ab2
Kevin Aguirre <kagr0410@icloud.com>
2024-12-31 15:44:04 -0600

    added nightmode feature, fixed formatting, structure and bugs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,68 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const themeBtn = document.getElementById("theme-btn");
+  
+    // Function to apply theme
+    const applyTheme = (theme) => {
+      
+        if (theme === "dark") {
+        
+            document.body.setAttribute("data-theme", "dark");
+            updateImgSourceBasedOffTheme(theme);
+        } else {
+            
+            document.body.setAttribute("data-theme", "light");
+            updateImgSourceBasedOffTheme(theme);
+      }
+    };
+  
+    // Detect system preference
+    const systemPreference = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    
+    // Get the user's saved theme from localStorage (if available)
+    const savedTheme = localStorage.getItem("theme");
+
+    const theme = savedTheme || systemPreference;
+    applyTheme(theme);
+  
+    // Toggle theme on button click
+    themeBtn.addEventListener("click", () => {
+      
+      const currentTheme = document.body.getAttribute("data-theme");
+      const newTheme = currentTheme === "dark" ? "light" : "dark";
+      applyTheme(newTheme);
+      localStorage.setItem("theme", newTheme); // Save the user's theme preference
+    });
+
+  });
+
+function updateImgSourceBasedOffTheme(theme) {
+
+    const githubIcon = document.getElementById("githubIcon");
+    const linkedinIcon = document.getElementById("linkedinIcon");
+    const arrowImg1 = document.getElementById("arrow1");
+    const arrowImg2 = document.getElementById("arrow2");
+    // const contactLinkedInIcon = document.getElementById("contact-linkedin-icon");
+    const contactEmailIcon = document.getElementById("contact-email-icon");
+
+    if (theme === "dark") {
+
+        githubIcon.src = "./assets/github-nightmode.png";
+        linkedinIcon.src = "./assets/linkedin-nightmode.png";
+        arrowImg1.src = "./assets/arrow-nightmode.png";
+        arrowImg2.src = "./assets/arrow-nightmode.png";
+        contactEmailIcon.src = "./assets/email-nightmode.png";
+    }
+    else {
+        
+        githubIcon.src = "./assets/github.png";
+        linkedinIcon.src = "./assets/linkedin.png";
+        arrowImg1.src = "./assets/arrow.png";
+        arrowImg2.src = "./assets/arrow.png";
+        contactEmailIcon.src = "./assets/email.png";
+    }
+}
+
 function toggleMenu() {
 
     const menu = document.querySelector(".menu-links");
@@ -7,22 +72,20 @@ function toggleMenu() {
 
 }
 
-function openResumeInWindow() {
-    const url = "../assets/resume-example.pdf";
-    window.open(url);
-}
-
 function goToSection() {
+    
     const url = "./#contact";
     window.location.href = url;
 }
 
 function goToLinkedin() {
+    
     const url = "https://www.linkedin.com/in/kevinaguirre1";
     window.location.href = url;
 }
 
 function goToGithub() {
+    
     const url = "https://github.com/kevinaguirredev";
     window.location.href = url;
 }
@@ -44,9 +107,7 @@ function updateYearCopyright() {
 addClickEventListener("hambgr-icon", "click", toggleMenu);
 addClickEventListener("hamburger-about", "click", toggleMenu);
 addClickEventListener("hamburger-exp", "click", toggleMenu);
-// addClickEventListener("hamburger-proj", "click", toggleMenu);
 addClickEventListener("hamburger-contact", "click", toggleMenu);
-// addClickEventListener("resumeBtn", "click", openResumeInWindow);
 addClickEventListener("contactBtn", "click", goToSection);
 addClickEventListener("linkedinIcon", "click", goToLinkedin);
 addClickEventListener("githubIcon", "click", goToGithub);
